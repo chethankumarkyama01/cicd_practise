7f324f3c2a681874fdc3a4d49de361c39e228c19
chethankumarkyama01 <chethankumarkyama@gmail.com>
2024-10-03 11:22:19 +0530

    login locators

---
+++ locators/login.ts
@@ -0,0 +1,12 @@
+export class Login{
+    static username: string = "input#username";
+    static password: string = "input#password";
+    static submit: string = "button#submit";
+    static login_success: string = "h1.post-title";
+}
+
+export class LoginDetails{
+    static username: string = "student";
+    static password: string = "Password123";
+    static login_success: string = "Logged In Successfully";
+}
